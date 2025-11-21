698c63c6b5ed808534dc072167c6903b5486c732
Ron Rise <ron@siteworxpro.com>
2025-11-21 09:15:23 -0500

    Refactor ESLint configuration to use defineConfig for improved clarity

--- js/eslint.config.ts
+++ js/eslint.config.ts
@@ -3,8 +3,9 @@ import js from "@eslint/js";
 // @ts-ignore
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import {defineConfig} from "eslint/config";
 
-export default [
+export default defineConfig([
     js.configs.recommended,
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
